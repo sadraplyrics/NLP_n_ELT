ac561264cea4efb4cb81615974932aec67226abc
sadraplyrics <dancingfallingleaves@gmail.com>
2023-07-03 12:16:14 +0300

    final final

--- lexicon/lexicon.py
+++ lexicon/lexicon.py
@@ -1,3 +1,7 @@
 LEXICON = {
-    "/start": "Sup!\nMy name is NLP_ELT_bot",
+    """/start": "Sup!\nMy name is NLP_ELT_bot\nHere is the list of available commands:\n
+    /words - the command that activates learning interface\n
+    /nums_facts - the command that allows you to learn interesting fact about a prompted number\n
+    /known - command that shows the list of lates 5 words you learned
+    """,
 }
